f1bc6c82a2f3d6caf3ba9c4de8e769fd8de497fe
tochman <thomas@craftacademy.se>
2016-12-17 08:50:05 +0100

    modifies the template to display comments

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -7,5 +7,6 @@ class LandingController < ApplicationController
     #binding.pry
     article = Article.find(params[:article])
     article.comments.create(comment: params[:comment])
+    redirect_to root_path
   end
 end
